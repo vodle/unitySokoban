fbd0f83030f480cf0ada53f734dcf6314367d0f7
vodle <thexhbik@gmail.com>
2022-01-20 20:25:51 +0300

    first commit

---
+++ Assets/Scripts/FinalPositionController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class FinalPositionController : MonoBehaviour
+{
+    private static int gameScore = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(gameObject.tag))
+        {
+            gameScore++;
+            gameObject.SetActive(false);
+            other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            if (gameScore == gameObject.transform.parent.transform.childCount)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Player : MonoBehaviour
+{
+    public int speed;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Movement();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Walls")
+            return;
+        if (collision.gameObject.name == "Map Wall")
+            return;
+        Rigidbody collisionObject = collision.gameObject.GetComponent<Rigidbody>();
+        Vector3 velocityOfPlayer = gameObject.GetComponent<Rigidbody>().velocity;
+        collisionObject.AddForce(velocityOfPlayer);
+    }
+
+    private void Movement()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        GetComponent<Rigidbody>().velocity = movement * speed;
+    }
+}
